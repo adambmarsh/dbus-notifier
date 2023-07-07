084a1a775f4c519c8479801ef2dadea674baf0e0
Adam Bukolt <abukolt@zoho.com>
2023-07-07 15:48:32 +0100

    update requirements, new build

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
 
 setuptools.setup(
     name="dbus_notifier",
-    version="0.1.3",
+    version="0.1.4",
     author="Adam Bukolt",
     author_email="abukolt@gmx.com",
     description="Package to post dbus notifications on Linux",
